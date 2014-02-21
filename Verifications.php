ef6e2e30a346dc8eecd136ee705ab1ec7e0c9b15
Kevin Hardy <h.kevin@neuf.fr>
2014-02-21 14:37:11 +0100

    Inscriptions des champs dans un fichier

--- Verifications.php
+++ Verifications.php
@@ -8,75 +8,87 @@
         
         if($_POST['prenom'] == '') {
             $aerror[] = '<p>Veuillez renseigner un prénom.<p>';
+        }else {
+        ecriture($_POST['prenom']);
         }
-        
     }
     
     if(isset($_POST['nom']) == true) { //Teste si le champs est vide.
         
         if($_POST['nom'] == '') {
             $aerror[] = '<p>Veuillez renseigner un nom.<p>';
+        }else {
+        ecriture($_POST['nom']);
         }
-        
     }
     if(isset($_POST['date']) == true) { //Teste si le champs est vide.
         
         if($_POST['date'] == '') {
             $aerror[] = '<p>Veuillez renseigner une date.<p>';
+        }else {
+        ecriture($_POST['date']);
         }
-        
     }
     if(isset($_POST['sexe']) == true) { //Teste si le champs est vide.
         
         if($_POST['sexe'] == 'Sélectionnez') {
             $aerror[] = '<p>Veuillez renseigner votre sexe.<p>';
+        }else {
+        ecriture($_POST['sexe']);
         }
-        
     }
      if(isset($_POST['email']) == true) { //Teste si le champs est vide.
         
         if($_POST['email'] == '') {
             $aerror[] = '<p>Veuillez renseigner une adresse email.<p>';
+        }else {
+        ecriture($_POST['email']);
         }
-        
     }
      if(isset($_POST['mdp']) == true) { //Teste si le champs est vide.
         
         if($_POST['mdp'] == '') {
             $aerror[] = '<p>Veuillez renseigner un mot de passe.<p>';
+        }else {
+        ecriture($_POST['mdp']);
         }
-        
     }
     if(isset($_POST['textarea']) == true) { //Teste si le champs est vide.
         
         if($_POST['textarea'] == '') {
             $aerror[] = '<p>Veuillez renseigner une description de vous-même.<p>';
+        }else {
+        ecriture($_POST['textarea']);
         }
-        
     }
     
     if(isset($_POST['radio']) == false) { //Teste si le champs est vide.
         
-       
             $aerror[] = 'Veuillez renseigner votre situation professionnelle.';
-        
-            
-    }
+    }else {
+        ecriture($_POST['radio']);
+        }
+    
     
     if(isset($_POST['mdp2']) == true) { //Teste si le champs est vide.
         
         if($_POST['mdp2'] == '') {
             $aerror[] = '<p>Veuillez confirmer votre mot de passe.<p>';
+        }else {
+        ecriture($_POST['mdp2']);
         }
-        
-    }    
+    }  
     
     if(isset($_POST['checkbox[]']) == false) { //Teste si le champs est vide.
         
-            $aerror[] = '<p>Veuillez cocher au moins une case.<p>';  
-    }
+            $aerror[] = 'Veuillez cocher au moins une case.';  
+    }else {
+        ecriture($_POST['checkbox[]']);
+        }
+   }   
     
-    }   
-    
+  function ecriture($contenu) {
+    file_put_contents("fichier.txt", $contenu . "\r\n", FILE_APPEND);
+}
 ?>
 
